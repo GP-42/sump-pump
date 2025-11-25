6e70797cee521f34d5e475be3f6c6fd8f963f69a
GP-42 <63603461+GP-42@users.noreply.github.com>
2025-11-25 18:15:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages  
- 
-setup(  
-    name="sump",  # Name used for import (e.g., "import my_package")  
-    version="0.0.1",  
-    packages=find_packages(where="src"),  # Finds packages under src/  
-    package_dir={"": "src"},  # Tells setuptools packages are in src/  
+from setuptools import setup, find_packages
+
+setup(
+    name="sump",  # Name used for import (e.g., "import my_package")
+    version="0.0.1",
+    packages=find_packages(where="src"),  # Finds packages under src/
+    package_dir={"": "src"},  # Tells setuptools packages are in src/
     install_requires=[
         "blinkt == 0.1.2",
         "buttonshim == 0.0.2",
@@ -13,5 +13,5 @@ setup(
         "pyserial == 3.5",
         "python-dotenv == 1.1.1",
         "RPi.GPIO == 0.7.1"
-    ],  # Add dependencies here (e.g., "requests>=2.25.0")  
-)  
+    ],  # Add dependencies here (e.g., "requests>=2.25.0")
+)
